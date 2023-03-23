b591e154b31414463d07397366f33f7fd7bcad11
schhidal <49634856+schhidal@users.noreply.github.com>
2023-03-23 22:05:12 +0100

    Create my script.js

---
+++ my script.js
@@ -0,0 +1,11 @@
+To host a script file on GitHub, you can follow these steps:
+
+Create a GitHub account if you don't already have one.
+Create a new repository by clicking on the "+" sign on the top right corner of the GitHub homepage, and then select "New repository".
+Give your repository a name, a description (optional), and choose whether you want it to be public or private.
+Initialize the repository with a README.md file by selecting the checkbox for "Initialize this repository with a README".
+Create a new file by clicking on the "Add file" button, then selecting "Create new file".
+Name the file with a .js extension (e.g., myscript.js).
+Copy and paste the contents of your script file into the editor.
+Click on the "Commit changes" button to save your changes.
+Your script file is now hosted on GitHub, and you can access it by going to your repository's page and clicking on the file name.
